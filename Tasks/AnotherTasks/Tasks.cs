d62e3a68d33b110d09e8e2b9137bcf2c4dda0a34
Timur <timur.abdullin.23@mail.ru>
2025-09-27 22:31:04 +0300

    I made all homework

--- Tasks/AnotherTasks/Tasks.cs
+++ Tasks/AnotherTasks/Tasks.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 class Tasks
 {
-    /* Задача №1
+    /* Задача 1
     Дана последовательность из 10 чисел. Определить, является ли эта последовательность
     упорядоченной по возрастанию. В случае отрицательного ответа определить
     порядковый номер первого числа, которое нарушает данную последовательность.
@@ -13,11 +13,11 @@ class Tasks
      */
     public void IsSorted(int[] nums) // метод, который проверяет сортированный массив или нет
     {
-        bool isSorted = true;
+        bool isSorted = true; // переменная, которая будет хранить булевое значение, является ли массив сортированным или нет
 
         for (int i = 0; i < nums.Length - 1; i++)
         {
-            if (nums[i] >= nums[i + 1])
+            if (nums[i] > nums[i + 1])
             {
                 Console.WriteLine($"Порядковый номер первого числа, который нарушает последовательность: {i + 1}");
                 isSorted = false;
@@ -30,4 +30,149 @@ class Tasks
             Console.WriteLine("Отсортирован!");
         }
     }
+
+
+    /* Задача 2
+    Игральным картам условно присвоены следующие порядковые номера в зависимости от
+    их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.
+    Порядковые номера остальных карт соответствуют их названиям («шестерка»,
+    «девятка» и т. п.). По заданному номеру карты k (6 <=k <= 14) определить достоинство
+    соответствующей карты. Использовать try-catch-finally.
+     */
+    public void DefineValueOfCard()
+    {
+        Dictionary<int, string> cards = new Dictionary<int, string> // словарь для хранения значений 
+            {
+                { 6, "шестерка" },
+                { 7, "семерка" },
+                { 8, "восьмерка"},
+                { 9, "девятка" },
+                { 10, "десятка" },
+                { 11, "валет" },
+                { 12, "дама" },
+                { 13, "король" },
+                { 14, "туз" }
+            };
+
+
+        try
+        {
+            if (!int.TryParse(Console.ReadLine(), out int k))
+            {
+                throw new FormatException("Введите правильный формат числа!"); 
+            }
+
+            if (k < 6 || k > 14)
+            {
+                throw new IndexOutOfRangeException("Число k должно быть в рамках 6 <= k <= 14");
+            }
+
+            if (cards.TryGetValue(k, out string finalCard))
+            {
+                Console.WriteLine($"Достоинство карты {k}: {finalCard}");
+            }
+
+        } catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        } finally 
+        { 
+            Console.WriteLine("Все проверки завершены"); 
+        }
+    }
+
+    /* Задача 3
+    Напишите программу, которая принимает на входе строку и производит выходные
+    данные в соответствии с таблицей
+     */
+    public void PrintMatchingValue()
+    {
+        Console.Write("Введите строку: ");
+        string text = Console.ReadLine();
+
+        switch (text.ToLower())
+        {
+            case "jabroni":
+                Console.WriteLine("Patron Tequlla");
+                break;
+            case "school counselor":
+                Console.WriteLine("Anything with Alcohol");
+                break;
+            case "programmer":
+                Console.WriteLine("Hipster Craft Beer");
+                break;
+            case "bike gang member":
+                Console.WriteLine("Moonshine");
+                break;
+            case "politician":
+                Console.WriteLine("Your tax dollars");
+                break;
+            case "rapper":
+                Console.WriteLine("Cristal");
+                break;
+            default:
+                Console.WriteLine("Beer");
+                break;
+        }
+    }
+
+    /* Задача 4
+     Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,...,7) 
+    выводит на экран его название (понедельник, вторник, ..., воскресенье).
+    Использовать enum.
+     */
+    enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    public void PrintDayOfWeek()
+    {
+        Console.Write("Введите число(от 1 до 7): ");
+        
+        try
+        {
+            if (!int.TryParse(Console.ReadLine(), out int num))
+            {
+                throw new FormatException("Неверный формат ввода!");
+            }
+
+            if (num < 1 || num > 7)
+            {
+                throw new IndexOutOfRangeException("Число не входит в промежуток 1 <= num <= 7");
+            } 
+            Console.WriteLine((DayOfWeek)(num - 1));
+
+        } catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+    }
+
+    /* Задача 5
+    Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента
+    "Hello Kitty" или "Barbie doll" необходимо положить их в “сумку”, т.е. прибавить к
+    результату. Вывести на экран сколько кукол в “сумке”.
+     */
+    public int HowManyDollsInBag(string[] arrStrings)
+    {
+        int count = 0; // переменная счетчик
+        
+        foreach (var str in arrStrings)
+        {
+            if (str.Equals("Hello Kitty", StringComparison.CurrentCultureIgnoreCase) || str.Equals("Barbie doll", StringComparison.CurrentCultureIgnoreCase))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
--- Tasks/StartProgram.cs
+++ Tasks/StartProgram.cs
@@ -4,7 +4,13 @@
     {
         Tasks task = new Tasks();
         int[] nums = { 10, 2, 3, 4, 5, 6, 9, 10, -51, 0 };
+        string[] arr = { "hello kitty", "doll", "HeLlo Kitty", "Barbie doll", "awfawf", "gsgfbxvs", "BARBie DoLL" };
 
-        task.IsSorted(nums);
+
+        //task.IsSorted(nums); // Задача 1
+        //task.DefineValueOfCard(); // Задача 2
+        //task.PrintMatchingValue(); // Задача 3
+        //task.PrintDayOfWeek(); // Задача 4
+        Console.WriteLine($"Кукол в сумке: {task.HowManyDollsInBag(arr)}"); // Задача 5
     }
 }
